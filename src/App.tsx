62f2f30043d9fbdeccd2e94ad306f41d73cab251
zhaoning-pan <panzhaoning@outlook.com>
2025-05-28 11:35:39 +0800

    Create main entry point file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center pt-8">
+        Chinese Fortune Telling Website
+      </h1>
+    </div>
+  )
+}
+
+export default App
